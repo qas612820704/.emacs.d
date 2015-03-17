842ac70b27dde996c1fb207cc3bfede008287133
Lego <qas612820704@gmail.com>
2015-03-18 02:03:34 +0800

    Edit README and add irony-mode

--- README.md
+++ README.md
@@ -16,32 +16,58 @@ M-x eval-current-buffer RET
 ```
 
 ### 2. clone my setting
+
 ```bash
-$ cd $HOME/.emacs.d
-(if init.el exist)
-$ mv init.el init.el.orig 
-(fi)
-$ git clone git@github.com:qas612820704/myemacs.git
+# if you already have your own emacs setting
+# please backup
+$ cd $HOME
+$ mv .emacs.d/ .emacs_orig.d/
+
+$ git clone git@github.com:qas612820704/.emacs.d.git
 ```
 
-### 3. execute init.el
-```bash
-$ emacs -nw init.el
-```
-after using emacs to open your init.el
 
-```
-M-x eval-current-buffer RET ;; to get plug-in
+### 3. open emacs
+```bash
+$ emacs -nw
 ```
 waiting and enjoy!
 
-### 4. setup python
+### 4. setup python complete
+I use jedi to complete
+
+It need python package `virtualenv`
+
+if you use debian like me, execute
+```bash
+$ sudo aptitude install python-pip
+$ sudo pip install virtualenv
 ```
+get into emacs and run
+```lisp
 M-x jedi:install-server RET
 ```
-### 5. something else?
-```$ emacs``` is too long so my ```.zshrc``` add this line:
+### 5. setup irony-mode
+[irony-mode](https://github.com/Sarcasm/irony-mode) is auto complete 
+for c/cpp
+
+to see more information in the hyper link, I just say it simplely,
+
+install dependency
+```bash
+$ sudo aptitude install cmake 
 ```
+emacs run
+```lisp
+M-x irony-mode RET
+M-x irony-install-server RET
+```
+Usage : (M-RET) to call the complete
+
+
+### 6. something else?
+`$ emacs` is too long so my `.zshrc` add this line:
+```bash
 alias e='env TERM=xterm-256color emacs -nw'
 ```
 after setting, I use ```$ e``` to run emacs in terminal
--- init.el
+++ init.el
@@ -10,24 +10,78 @@
    (:name smex  
 	  :after (progn ;; 這邊就是對套件進行載入後設定，以及撰寫 recipe  
 		   (global-set-key (kbd "M-x") 'smex)  
-		   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))  
+		   (global-set-key (kbd "M-X") 'smex-major-mode-commands))
+	  )  
    (:name magit  
-          :after (global-set-key (kbd "C-x C-z") 'magit-status))
+          :after (global-set-key (kbd "C-x C-z") 'magit-status)
+	  )
    (:name jedi
 	  :after (progn
 		   (add-hook 'python-mode-hook 'jedi:setup)
 		   (setq jedi:complete-on-dot t)
-		   ))
+		   )
+	  )
    (:name neotree
-	  :after (global-set-key [f8] 'neotree-toggle))
-   ))  
+	  :after (global-set-key [f8] 'neotree-toggle)
+	  )
+   (:name android-mode
+	  :after (custom-set-variables
+		  '(android-mode-sdk-dir
+		    "/usr/local/src/android-sdk-linux")
+		  )
+	  )
+   (:name evil
+	  :type github
+	  :pkgname "emacsmirror/evil"
+	  )
+   (:name emmet-mode
+	  :after (progn
+		   (add-hook 'web-mode-hook 'emmet-mode)
+		   (add-hook 'php-mode-hook 'emmet-mode)
+		   )
+	  )
+   (:name irony-mode
+	  :after (progn
+		   (add-hook 'c++-mode-hook 'irony-mode)
+		   (add-hook 'c-mode-hook 'irony-mode)
+		   (add-hook 'objc-mode-hook 'irony-mode)
+
+		   ;; replace the `completion-at-point' and `complete-symbol' bindings in
+		   ;; irony-mode's buffers by irony-mode's function
+		   (defun my-irony-mode-hook ()
+		     (define-key irony-mode-map [remap complete-symbol]
+		       'irony-completion-at-point-async)
+		     (define-key irony-mode-map [remap complete-symbol]
+		       'irony-completion-at-point-async))
+		   (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+		   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+		   )
+	  )
+   (:name ac-irony
+	  :after (progn
+		   (defun my-ac-irony-setup ()
+		     ;; be cautious, if yas is not enabled before (auto-complete-mode 1), overlays
+		     ;; *may* persist after an expansion.
+		     ;; (yas-minor-mode 1)
+		     (auto-complete-mode 1)
+		     
+		     (add-to-list 'ac-sources 'ac-source-irony)
+		     (define-key irony-mode-map (kbd "M-RET") 'ac-complete-irony-async))
+		   
+		   (add-hook 'irony-mode-hook 'my-ac-irony-setup)
+		   )
+	  )
+ ))  
 
 ;; 設定要安裝的包！這裡是重點，下面解釋：  
 (setq packages  
       (append  
        '(el-get smex magit web-mode tomorrow-theme
-		jedi android-mode neotree)  
-       (mapcar 'el-get-source-name el-get-sources)))  
+		jedi android-mode neotree
+		tabbar php-mode evil emmet-mode
+		irony-mode ac-irony
+		)  
+       (mapcar 'el-get-source-name el-get-sources)))
 
 ;; 這裡是設定重點，包含異步/同步啟動機制  
 (el-get 'sync packages)  
@@ -43,3 +97,6 @@
 (scroll-bar-mode -1)
 (line-number-mode 1)
 (speedbar 1)
+
+;; evil-mode enable
+(evil-mode 1)
